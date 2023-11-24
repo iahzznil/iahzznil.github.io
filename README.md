725aa68c2af3c82046989ed7443208ef234211d4
ryan <ryan2022022@gmail.com>
2023-11-24 18:25:14 +0400

    source code commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# cool blog 
+
+cool blog forked from [https://github.com/varHarrie/varharrie.](https://github.com/varHarrie/varharrie.)
+
+
+## License
+
+[MIT](./LICENSE)
---
+++ src/App.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { HashRouter, Outlet, Route, Routes } from 'react-router-dom';
+
+import Article from './views/Article';
+import Articles from './views/Articles';
+import Home from './views/Home';
+import Main from './views/Main';
+import Projects from './views/Projects';
+
+const milestones = {
+  posts: Number(import.meta.env.VITE_GITHUB_MILESTONE_POSTS),
+  snippets: Number(import.meta.env.VITE_GITHUB_MILESTONE_SNIPPETS),
+  essays: Number(import.meta.env.VITE_GITHUB_MILESTONE_ESSAYS),
+};
+
+export default function App() {
+  return (
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<Main />}>
+          <Route path="" element={<Home />} />
+          <Route path="posts" element={<Outlet />}>
+            <Route path="" element={<Articles milestone={milestones.posts} />} />
+            <Route path=":id" element={<Article />} />
+          </Route>
+          <Route path="essays" element={<Outlet />}>
+            <Route path="" element={<Articles milestone={milestones.essays} />} />
+            <Route path=":id" element={<Article />} />
+          </Route>
+          <Route path="snippets" element={<Outlet />}>
+            <Route path="" element={<Articles milestone={milestones.snippets} />} />
+            <Route path=":id" element={<Article />} />
+          </Route>
+          <Route path="projects" element={<Projects />} />
+        </Route>
+      </Routes>
+    </HashRouter>
+  );
+}
